abf1dd383ac9f0d8ab946f8d482b8e2573a65da3
reyx-web <riyariy909@gmail.com>
2025-07-04 13:37:26 +0530

    add all files

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html", "./src/**/*.{{html,js,jsx,ts,tsx,vue,astro}"],
+  theme: {
+    extend: {
+      transitionProperty: {
+        "background-position": "background-position",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+    require("prettier-plugin-tailwindcss"),
+    require("@material-tailwind/html/utils/withMT"),
+    require("@tailwindcss/deprecation-warnings"),
+  ],
+};
