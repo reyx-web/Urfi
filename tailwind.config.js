f60932bec15b7b01c6d586770b1c1fa24e91169a
Riya <riyariy909@gmail.com>
2025-07-24 14:11:41 +0000

    fully refreshed content and added internal links to all bangalore pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.html", "./src/**/*.{{html,js,jsx,ts,tsx,vue,astro}"],
+  content: ["./**/*.html", "./public/**/*.{{html,js,jsx,ts,tsx,vue,astro}"],
   theme: {
     extend: {
     },
