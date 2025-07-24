f0816f4b7d5c40eafd6840f3844770be90d02a96
Riya <riyariy909@gmail.com>
2025-07-24 14:39:26 +0000

    lil file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.html", "./public/**/*.{{html,js,jsx,ts,tsx,vue,astro}"],
+  content: [
+    "./public/**/*.html",
+    "./src/**/*.{html,js,jsx,ts,tsx,vue,astro}"
+  ],
   theme: {
-    extend: {
-    },
+    extend: {},
   },
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/container-queries"),
-    require("prettier-plugin-tailwindcss"),
     require("@material-tailwind/html/utils/withMT"),
-    require("@tailwindcss/deprecation-warnings"),
   ],
 };
